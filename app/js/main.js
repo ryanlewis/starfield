1bb3bef462f7dcd48f6bdf03c7107a5996a13cb3
Ryan Lewis <ryan@wpyz.org>
2014-05-30 23:11:58 +0100

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,139 @@
+require({
+        baseUrl: 'js',
+    // three.js should have UMD support soon, but it currently does not
+    shim: { 'vendor/three': { exports: 'THREE' } }
+}, [
+    'vendor/three'
+], function(THREE) {
+
+var camera, scene, renderer;
+var starGeometry, material, mesh, starSprite;
+var mouseX = 0, mouseY = 0, wheelDelta = 0, zVel = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    // camera.position.z = 1000;
+    camera.position.z = 0;
+    window.camera = camera;
+
+    scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
+
+    starGeometry = new THREE.Geometry();
+
+    // set the material for each star
+    // TODO: make a sprite
+    starSprite = THREE.ImageUtils.loadTexture( "img/sprites/star.png" );
+	material = new THREE.ParticleSystemMaterial( { color: 0xffffff, size: 20, sizeAttenuation: true, map: starSprite, blending: THREE.AdditiveBlending, depthTest: true, transparent: true } );
+    //material.color.setHSL( 1.0, 1, 1 );
+
+    // create all our star vertices from the API
+    createStarVertices();
+
+    // create the particle system, that will use our star vertices
+    particles = new THREE.ParticleSystem( starGeometry, material );
+    particles.sortParticles = true;
+    scene.add( particles );
+
+    // lets create a renderer that takes up the whole screen
+    //renderer = new THREE.CanvasRenderer({clearAlpha: 1});
+    renderer = new THREE.WebGLRenderer( { clearAlpha: 1 } );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // add to the dom
+    document.body.appendChild(renderer.domElement);
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
+	// document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+	// document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function createStarVertices() {
+
+    for ( i = 0; i < 10000; i ++ ) {
+    	var vertex = new THREE.Vector3();
+    	vertex.x = 2000 * Math.random() - 1000;
+    	vertex.y = 2000 * Math.random() - 1000;
+    	// vertex.z = 2000 * Math.random() - 1000;
+        vertex.z = (2000 * Math.random()) - 1000;
+
+    	starGeometry.vertices.push( vertex );
+    }
+}
+
+function moveCamera() {
+
+    var MAXSPEED = 0.5;
+
+    var vx = mouseX/500;
+    var vy = mouseY/500;
+
+    if (Math.abs(mouseX) < 50 && Math.abs(mouseY) < 50) {
+        vx = 0; vy = 0;
+    }
+
+    console.log(vy);
+    camera.position.x += clamp(vx, MAXSPEED);
+    camera.position.y -= clamp(vy, MAXSPEED);
+
+    if (wheelDelta != 0) {
+
+        if (wheelDelta > 0)
+            camera.position.z += wheelDelta * 0.05;
+        else
+            camera.position.z += wheelDelta * 0.05;
+    }
+}
+
+function clamp (v, d) {
+    if (v > 0) {
+        return Math.min(v, d);
+    } else {
+        return Math.max(v, d * -1);
+    }
+}
+
+function animate() {
+
+    // note: three.js includes requestAnimationFrame shim
+    requestAnimationFrame(animate);
+
+    moveCamera();
+    renderer.render(scene, camera);
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = event.clientX - windowHalfX;
+	mouseY = event.clientY - windowHalfY;
+}
+
+function onDocumentMouseWheel( event ) {
+    wheelDelta = event.wheelDelta || event.detail;
+
+    // macbook touchpad just seems to keep going at 3 or -3
+    if (Math.abs(wheelDelta) < 5) wheelDelta = 0;
+}
+
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+});
